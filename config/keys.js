f827d03095d5f0080d00c4a2f16e6e9bdfd7858d
Song <yousonghou@yahoo.com>
2018-05-28 23:06:11 +0000

    add connection to MongoDB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://song:12345@ds237700.mlab.com:37700/devc'
+};
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
 app.get('/', (req, res) => res.send('Hello World'));
 
-// process.env.PORT for Heroku, port 5000 for local
-const port = process.env.PORT || 5000;
+// process.env.PORT for Heroku, port 8080 for local
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
