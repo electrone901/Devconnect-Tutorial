9c9c77c8cef9c5f713397c4e258af8fe9f5b5f4d
Song <yousonghou@yahoo.com>
2018-05-29 22:50:00 +0000

    add user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load User Model
+const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Users Work"}));
+
+// @route   GET api/users/register
+// @desc    Register users
+// @access  Public
+router.post('/register', (req, res) => {
+    // search for email if it match
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(user){
+                return res.status(400).json({email: 'Email Already Exists'});
+            }
+            else{
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', // Size
+                    r: 'pg', // Rating
+                    d: 'mm' // Defalt
+                });
+                
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                });
+                
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        // hash the password
+                        newUser.password = hash;
+                        newUser
+                            .save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    });
+                });
+            }
+        });
+});
 
 module.exports = router;
