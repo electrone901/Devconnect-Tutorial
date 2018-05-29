802d72a194eca35c8b8b43d8b692b891e82aca80
Song <yousonghou@yahoo.com>
2018-05-29 19:19:57 +0000

    add the user model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+// the convention for models is singular and start with a capital
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
