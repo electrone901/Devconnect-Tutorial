e18b8cc32167221c215324710992147ffde45eec
Song <yousonghou@yahoo.com>
2018-06-30 15:37:00 +0000

    add dashboard start

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
@@ -10,7 +11,7 @@ class Dashboard extends Component{
     }
     
     render(){
-        //const {user} = this.props.auth;
+        const {user} = this.props.auth;
         const {profile, loading} = this.props.profile;
         
         let dashboardContent;
@@ -19,7 +20,22 @@ class Dashboard extends Component{
             dashboardContent = <Spinner />;
         }
         else{
-            dashboardContent = <h4>Hello</h4>;
+            // Check if logged in user has profile data
+            if(Object.keys(profile).length > 0){
+                dashboardContent = <h4>DISPLAY PROFILE</h4>;
+            }
+            else{
+                // User is logged in but has no profile
+                dashboardContent = (
+                    <div>
+                        <p className="lead text-muted">Welcome {user.name}</p>
+                        <p>You have not yet setup a profile, please add info</p>
+                        <Link to="/create-profile" className="btn btn-lg btn-info">
+                            Create Profile
+                        </Link>
+                    </div>
+                );
+            }
         }
         
         return(
