b0a39dfc881c88b350d94f4d09d8131162e5ec11
Song <yousonghou@yahoo.com>
2018-07-18 18:49:01 +0000

    add getProfiles action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from '../axios-lists';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
+import { GET_PROFILE, GET_PROFILES, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Get  current profile
 export const getCurrentProfile = () => dispatch => {
@@ -113,6 +113,25 @@ export const deleteAccount = () => dispatch => {
                 })
             );
     }
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+    dispatch(setProfileLoading());
+    axios
+        .get('/api/profile/all')
+        .then(res =>
+            dispatch({
+                type: GET_PROFILES,
+                payload: res.data
+            })
+        )
+        .catch(err => 
+            dispatch({
+                type: GET_PROFILES,
+                payload: null
+            })
+        );
 };
 
 // Profile loading
