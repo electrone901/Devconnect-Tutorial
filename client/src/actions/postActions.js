b70e6bfb79b09735ceb6a4495c6c0c2cda98f238
Song <yousonghou@yahoo.com>
2018-08-05 01:47:11 +0000

    fix axios spelling mistake and add PostFeed

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from '../axios-lists';
 
 import{
     ADD_POST,
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import PostForm from './PostForm';
-//import Spinner from '../common/Spinner';
+import PostFeed from './PostFeed';
+import Spinner from '../common/Spinner';
+
 import { getPosts } from '../../actions/postActions';
 
 class Posts extends Component{
@@ -11,12 +13,23 @@ class Posts extends Component{
     }
     
     render(){
+        const {posts, loading} = this.props.post;
+        let postContent;
+        
+        if(posts === null || loading){
+            postContent = <Spinner />;
+        }
+        else{
+            postContent = <PostFeed posts={posts} />;
+        }
+        
         return(
             <div className="feed">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12">
                             <PostForm />
+                            {postContent}
                         </div>
                     </div>
                 </div>
