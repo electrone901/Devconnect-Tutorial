963f51d8f4eb329f0e5e881c6e5389c58ddeb3fb
Song <yousonghou@yahoo.com>
2018-08-16 22:22:50 +0000

    add clear error action and reducer

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,7 @@ import axios from '../axios-lists';
 import{
     ADD_POST,
     GET_ERRORS,
+    CLEAR_ERRORS,
     GET_POSTS,
     GET_POST,
     POST_LOADING,
@@ -11,6 +12,7 @@ import{
 
 // Add Post
 export const addPost = postData => dispatch => {
+    dispatch(clearErrors());
     axios
         .post('/api/posts', postData)
         .then(res => 
@@ -111,6 +113,7 @@ export const removeLike = id => dispatch => {
 
 // Add Comment
 export const addComment = (postId, commentData) => dispatch => {
+    dispatch(clearErrors());
     axios
         .post(`/api/posts/comment/${postId}`, commentData)
         .then(res => 
@@ -149,5 +152,12 @@ export const deleteComment = (postId, commentId) => dispatch => {
 export const setPostLoading = () => {
     return{
         type: POST_LOADING
+    };
+};
+
+// Clear errors
+export const clearErrors = () => {
+    return{
+        type: CLEAR_ERRORS
     };
 };
