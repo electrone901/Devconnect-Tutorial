615cdbc71451380d90f1c8cb936b019e9443e2e2
Song <yousonghou@yahoo.com>
2018-08-18 03:31:04 +0000

    fix mixed content blocked issues

--- client/src/axios-lists.js
+++ client/src/axios-lists.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-   baseURL: 'http://devconnectpractice-ysongh.c9users.io:8081' 
+   baseURL: 'https://devconnectpractice-ysongh.c9users.io:8081' 
 });
 
 export default instance;
